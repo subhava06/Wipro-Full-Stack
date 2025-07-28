78a6706135f350d1070592b3b59370eeb487ce3d
subhava06 <subhav.22gcebcs190@galgotiacollege.edu>
2025-07-28 13:44:22 +0530

    wipro basics

---
+++ Flow control statements/PrintLetters.java
@@ -0,0 +1,15 @@
+
+
+public class PrintLetters {
+
+    public static void main(String[] args) {
+        
+        int[] arr = new int[100];
+        for(int i = 65; i<91;i++)
+        {
+         char ch = (char) arr[i];
+         System.out.print(ch+" ");
+        }
+    }
+    
+}
